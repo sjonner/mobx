4018339d4f2566677b30bcd4e3636a5df7a0c471
John van Keulen <sjonner@gmail.com>
2017-08-02 12:22:38 +0200

    Support observableMap in localStorage

--- src/storedObservable.js
+++ src/storedObservable.js
@@ -1,19 +1,23 @@
-import { observable, autorunAsync } from 'mobx';
-import { merge, cloneDeep } from 'lodash';
+import { isObservableMap, observable, autorunAsync, extendObservable } from 'mobx';
+import { mergeWith } from 'lodash';
 
 export default function storedObservable(key, defaultValue, debounce) {
   const initialState = localStorage.getItem(key);
-  const hydrated = cloneDeep(defaultValue);
+  const hydrated = extendObservable(observable({}), defaultValue);
 
   if (initialState) {
-    merge(hydrated, JSON.parse(initialState));
+    mergeWith(hydrated, JSON.parse(initialState), checkObservables);
   }
 
-  const hydratedObservable = observable(hydrated);
-
   autorunAsync(() => {
-    localStorage.setItem(key, JSON.stringify(hydratedObservable));
+    localStorage.setItem(key, JSON.stringify(hydrated));
   }, debounce);
 
-  return hydratedObservable;
+  return hydrated;
+}
+
+function checkObservables(objValue, srcValue, key) {
+  if (isObservableMap(objValue)) {
+    return objValue.merge(srcValue);
+  }
 }
