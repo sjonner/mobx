9563d8dda2e00bb2e1a6543c59d3257dae478b43
John van Keulen <sjonner@gmail.com>
2017-08-02 12:46:08 +0200

    Enable mobx strict mode

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,8 @@
+import { useStrict } from 'mobx';
 // import UiStore from './UiStore'
 import CounterStore from './Counter/CounterStore';
 
+useStrict(true);
   // ui: new UiStore(),
   counter: new CounterStore(),
 
