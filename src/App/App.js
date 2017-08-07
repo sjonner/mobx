6c700308bed8df3d1fe36c15ef36d6acd5e6025c
John van Keulen <sjonner@gmail.com>
2017-08-07 09:54:23 +0200

    Remove logo

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import Todo from '../Todo/Todo';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   counterModule = null;
 
   render() {
-    console.log('render');
     return (
       <div className="App">
         {this.props.children}
-
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
 
         <div className="App-intro">
           <Todo />
